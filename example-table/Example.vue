af3593ad96b97969b497c7d36f40166533603a0a
Artur Tagisow <artur@tagisow.dev>
2022-02-08 00:54:35 +0100

    rewrite to script setup

--- example-table/Example.vue
+++ example-table/Example.vue
@@ -9,22 +9,12 @@
   </MyTable>
 </template>
 
-<script>
+<script setup>
 import MyTable from './MyTable.vue'
 import MyTableColumn from './MyTableColumn.vue'
-export default {
-  name: 'HelloWorld',
-  components: {
-    MyTable,
-    MyTableColumn
-  },
-  data() {
-    return {
-      items: [
-        {toyName: 'hotwheels', price: 700},
-        {toyName: 'doll', price: 500},
-      ]
-    }
-  }
-}
+
+const items = [
+  {toyName: 'hotwheels', price: 700},
+  {toyName: 'doll', price: 500},
+]
 </script>
